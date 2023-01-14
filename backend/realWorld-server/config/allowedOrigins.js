1649a72838414ff4aa9aa5e88de87993a2851c88
Siya Pathak <98201597+siya-pathak@users.noreply.github.com>
2023-01-14 20:44:59 +0530

    Delete backend directory

--- backend/realWorld-server/config/allowedOrigins.js
+++
@@ -1,8 +0,0 @@
-const allowedOrigins = [
-    'http://localhost:3000',
-    'http://localhost:4200',
-    'https://winterrrrrff.github.io',
-    'https://angular-realworld-example-app-neon.vercel.app'
-];
-
-module.exports = allowedOrigins;
